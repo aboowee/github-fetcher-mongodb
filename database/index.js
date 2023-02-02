5d792e76023786aae496b3463d3b61eede95c534
Anthony Bui <anthonyb50@gmail.com>
2023-02-01 20:34:38 -0500

    Complete save function for database

--- database/index.js
+++ database/index.js
@@ -3,26 +3,34 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
-  _id: Number,
-  username: String,
-  repoName: String,
-  repoUrl: String,
-  //numberOfForks: Number  IF ORDERING BY NUMBER
-  updatedAt: Date,
-  //If NOT updated, use created?
-  createdAt: Date,
-  starGazerCount: Number
-
-
+  _id: {type: Number, unique: true, required: true},
+  username: {type: String, required: true},
+  repoName: {type: String, required: true},
+  repoUrl: {type: String, required: true},
+  description: {type: String, required: true},
+  // updatedAt: Date,
+  // createdAt: Date,
+  forks: {type: Number, required: true}
 
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (dataSet) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  dataSet.forEach((currentRepo) => {
+
+    let dataToAdd = new Repo({_id: currentRepo.id, username: currentRepo.owner.login, repoName: currentRepo.name, repoUrl: currentRepo.html_url, description: currentRepo.description, forks: currentRepo.forks})
+
+    dataToAdd.save((error) => {
+      if (error) {
+        console.log('Ran into error');
+        return error;
+      }
+    })
+  })
 }
 
 module.exports.save = save;
