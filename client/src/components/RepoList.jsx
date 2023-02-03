f380ed057934d329e7d17a34f719cf7c94ba3eae
Anthony Bui <anthonyb50@gmail.com>
2023-02-02 23:17:46 -0500

    Complete displaying top 25 repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,6 +4,18 @@ const RepoList = ({ repos }) => (
   <div>
     <h4> Repo List Component </h4>
     There are {repos.length} repos.
+    {
+      repos.map((repo) => {
+        return(
+        <div key={repo.repoId}>
+          <li>Name: {repo.repoName}</li>
+          <li>Link to Repo: {repo.repoUrl}</li>
+          <li>Created By: {repo.username}</li>
+          <li>Forks: {repo.forks}</li>
+        </div>
+        )
+      })
+    }
   </div>
 )
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,25 +9,53 @@ const App = () => {
   const [repos, setRepos] = useState([]);
 
   const search = (term) => {
-    console.log(`${term} was searched`);
+    // console.log(`${term} was searched`);
 
     $.ajax({
       method: 'POST',
       url: '/repos',
       dataType: 'json',
       contentType: "application/json; charset=utf-8",
-      data: JSON.stringify({username: term})
+      data: JSON.stringify({username: term}),
+      success: () => {
+        console.log('Username was added');
+        receiveData();
+      },
+      error: () => {
+        console.log('Username was not added');
+        receiveData();
+      }
     })
-    .done(function(result) {
-      console.log('Username was added:  ', result);
+    // .done(function(result) {
+    //   console.log('Username was added:  ', result);
+
+    // })
+  }
+
+  const receiveData = () => {
+
+    $.ajax({
+      method: 'GET',
+      url: '/repos',
+      dataType: 'json',
+      success: (data) => {
+        console.log('Got Data:  ', data);
+        setRepos(data);
+      },
+      error: (error) => {
+        console.log('No data:   ', error);
+      }
     })
+    // .done(function(result) {
+    //   console.log('Username was added:  ', result);
+    // })
   }
 
   return (
     <div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={repos}/>
       <Search onSearch={search}/>
+      <RepoList repos={repos}/>
     </div>
   );
 }
